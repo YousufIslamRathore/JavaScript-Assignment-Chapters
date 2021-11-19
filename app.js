3110ec8ed28f77a9776c7551dbeb39bee0e885cd
Yousuf Islam Rathore <rathoreislam@gmail.com>
2021-11-20 04:31:59 +0500

    Work Complete

--- app.js
+++ app.js
@@ -1408,3 +1408,92 @@
 // var num = +prompt("Enter any positive integer")
 // var fact = factorial(num)
 // document.write("Factorial of " + num + " is " + fact)
+
+
+// Question 07
+// var num1 = +prompt("Enter Starting Number")
+// var num2 = +prompt("Enter Last Number")
+// function count() {
+//     if (num1 <= num2) {
+//         for(var i = num1 ; i <= num2 ; i++) {
+//             document.write(i + "<br>")
+//         }
+//     }
+//     else {
+//         alert("Please Enter in Ascending Order")
+//     }
+// }
+// count()
+
+
+// Question 08
+// var base = +prompt("Enter Base of a Right Angled Triangle")
+// var perp = +prompt("Enter Perpendicular of a Right Angled Triangle")
+// function calculateHypotenuse(base, perp) {
+//     var hypo = Math.sqrt(base**2 + perp**2)
+//     return hypo
+// }
+// var hypotenuse = calculateHypotenuse(base, perp)
+// function calculateSquare() {
+//     var squared = hypotenuse**2
+//     return squared
+// }
+// var squaredHyp = calculateSquare()
+// document.write("Hypotenuse of your Right Angled Triangle is: " + hypotenuse)
+// document.write("<br> Square of the Hypotenuse is: " + squaredHyp)
+
+
+// Question 09
+// var width = +prompt("Enter Width")
+// var height = +prompt("Enter Height")
+// function calculateArea(width, height) {
+//     var area = width * height
+//     return area
+// }
+// var varArea = calculateArea(width, height)
+// var valArea = calculateArea(5, 6)
+// document.write("Area calculated from user's input i.e. Arguments as Variables : " + varArea + "<br><br>")
+// document.write("Area calculated from already given values i.e. Arguments as Values : " + valArea)
+
+
+// Question 10
+// var input = prompt("Enter any word to check if its a Palindrome")
+// function checkPalindrome(input) {
+//     var string = input.toLowerCase().replace(" ","")
+//     var count = 0;
+//     if (string === "") {
+//         alert("Nothing Found!")
+//     }
+//     if ((string.length) % 2 === 0){
+//         count = (string.length) / 2
+//     }
+//     else {
+//         if (string.length === 1) {
+//             document.write("Entered String is a Palindrome")
+//         }
+//         else {
+//             count = (string.length - 1) / 2
+//         }
+//     }
+//     for (var i = 0 ; i < count ; i++) {
+//         if (string[i] != string.slice(-1-i)[0]) {
+//             document.write("Entrered String is not a Palindrome")
+//             return false
+//         }
+//     }
+//     document.write("Entered String is a Palindrome")
+//     return true
+// }
+// checkPalindrome(input)
+
+
+// Question 11
+// var string = prompt("Enter any string")
+// function capitialize(string) {
+//     var arr = string.split(" ")
+//     for(var i = 0 ; i < arr.length ; i++){
+//         var word = arr[i][0].toUpperCase() + arr[i].slice(1).toLowerCase()
+//         document.write(word + " ")
+//     }
+// }
+// capitialize(string)
