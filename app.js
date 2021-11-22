0a4130944b86f4a08028c541878ab049bd1ba5bb
Yousuf Islam Rathore <rathoreislam@gmail.com>
2021-11-22 15:39:21 +0500

    Assignment Complete

--- app.js
+++ app.js
@@ -1574,3 +1574,57 @@
 // var area = calcArea(radius)
 // document.write("The circumference is " + circumference)
 // document.write("<br> The area is " + area)
+
+
+
+// Chapter 43 - 48
+
+// Question 01
+// function linkAlert() {
+//     alert("You have clicked on a link")
+// }
+
+
+// Question 02
+// function phoneAlert() {
+//     alert("Thanks for purchasing a phone from us")
+// }
+
+
+// Question 03
+// function deleteRow(element) {
+//     var i = element.parentNode.parentNode.rowIndex;
+//     document.getElementById("myTable").deleteRow(i);
+// }
+
+
+// Question 04
+// // 1st Method
+// function img1toimg2() {
+//     var img = document.getElementById("img1")
+//     img.src = "./images/img2.jpg"
+// }
+// function img2toimg1(e) {
+//     e.src = "./images/img1.jpg"
+// }
+//
+// // 2nd Method
+// function toggler(id, element) {
+//     if (id === 1) {
+//         element.src = "./images/img2.jpg"
+//     }
+//     else {
+//         element.src = "./images/img1.jpg"
+//     }
+// }
+
+
+// Question 05
+// function countInc() {
+//     var input = document.getElementById("input")
+//     input.value = eval(+input.value + 1)
+// }
+// function countDec() {
+//     var input = document.getElementById("input")
+//     input.value = eval(+input.value - 1)
+// }
