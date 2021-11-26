83f9004bf440b40df74c4dc8217e6e6d3880c813
Yousuf Islam Rathore <rathoreislam@gmail.com>
2021-11-27 02:55:25 +0500

    assignment complete

--- app.js
+++ app.js
@@ -1576,6 +1576,176 @@
 // document.write("<br> The area is " + area)
 
 
+// Chapter 38 - 44
+
+// Question 01
+// var a = +prompt("Enter the Base Value")
+// var b = +prompt("Enter the Exponent Value")
+// function power(a, b) {
+//     var result = Math.pow(a, b)
+//     return result
+// }
+// var result = power(a, b)
+// document.write("The value of " + a + " raised to " + b + " is " + result)
+
+
+// Question 04
+// var year = +prompt("Enter Year to check it's a leap year or not")
+// function checkLeapYear(year) {
+//     if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)){
+//         alert(year + " is a leap year!")    
+//     }
+//     else {
+//         alert(year + " is not a leap year!")
+//     }
+// }
+// checkLeapYear(year)
+
+
+// Question 03
+// // 1st Method
+// var a = +prompt("Length of side 1 of a triangle")
+// var b = +prompt("Length of side 2 of a triangle")
+// var c = +prompt("Length of side 3 of a triangle")
+// function calcSemiPerimeter(a, b, c) {
+//     var semiPerimeter = (a + b + c) / 2
+//     return semiPerimeter
+// }
+// var semiPerimeter = calcSemiPerimeter(a, b, c)
+// function calcArea(semiPerimeter, a, b, c,) {
+//     var area = Math.sqrt(semiPerimeter * (semiPerimeter - a) * (semiPerimeter - b) * (semiPerimeter - c))
+//     return area
+// }
+// var area = calcArea(semiPerimeter, a, b, c)
+// document.write("The Semi Perimeter is : " + semiPerimeter)
+// document.write("<br> The Area is : " + area)
+//
+// // 2nd Method
+// var a = +prompt("Length of side 1 of a triangle")
+// var b = +prompt("Length of side 2 of a triangle")
+// var c = +prompt("Length of side 3 of a triangle")
+// function calcArea(a, b, c) {
+//     function calcSemiPerimeter(a, b, c) {
+//         var semiPerimeter = (a + b + c) / 2
+//         return semiPerimeter 
+//     }
+//     var semiPerimeter = calcSemiPerimeter(a, b, c) 
+//     var area = Math.sqrt(semiPerimeter * (semiPerimeter - a) * (semiPerimeter - b) * (semiPerimeter - c))
+//     return area
+// }
+// var area = calcArea(a, b, c)
+// document.write("Area is : " + area)
+
+
+// Question 04
+// function mainFunction(sub1, sub2, sub3) {
+//     var sub1 = +prompt("Enter Subject 1 marks")
+//     var sub2 = +prompt("Enter Subject 2 marks")
+//     var sub3 = +prompt("Enter Subject 3 marks")
+//     function average(sub1, sub2, sub3) {
+//         var average = (sub1 + sub2 + sub3) / 3
+//         return average
+//     }
+//     var average = average(sub1, sub2, sub3)
+//     document.write("The Average is : " + average + "<br>")
+//     function percentage(sub1, sub2, sub3) {
+//         var obtMarks = sub1 + sub2 + sub3
+//         var totalMarks = 300
+//         var percentage = (obtMarks / totalMarks) * 100
+//         return percentage 
+//     }
+//     var percentage = percentage(sub1, sub2, sub3)
+//     document.write("The Percentage is : " + percentage)
+// }
+// mainFunction()
+
+
+// Question 05
+// var string = prompt("Enter any string")
+// var char = prompt("Enter the character that you want to get index of")
+// function indexof(string, char) {
+//     for (var i = 0 ; i < string.length ; i++) {
+//         if (string[i] == char) {
+//             return i
+//         }
+//     }
+// }
+// var indexNum = indexof(string, char)
+// document.write("First appearence of '" + char + "' in the string '" + string + "' : " + indexNum) 
+
+
+// Question 06
+// var string = prompt("Enter String : (length should not be more than 25)")
+// var vowels = ["a", "e", "i", "o", "u"]
+// function vowelDeleter(string) {
+//     for (var i = 0; i < vowels.length; i++) {
+//         var arr = string.split(vowels[i])
+//         string = arr.join("")
+//     }
+//     return string
+// }
+// var unVoweled = vowelDeleter(string)
+// document.write("Your Entered String without any Vowels : " + unVoweled)
+
+
+// Question 07
+// function findOccurances(str){
+//     var words = str.split(" ");
+//     var count=0;
+//     for(var i=0;i<words.length;i++){
+//         for(var j=0; j<words[i].length; j++){
+//             var char = words[i].slice(j,j+1).toLowerCase();
+//             var nextChar = words[i].slice(j+1,j+2).toLowerCase();
+//             switch(char){
+//                 case "a":
+//                 case "e":
+//                 case "i":
+//                 case "o":
+//                 case "u":
+//                     switch(nextChar){
+//                         case "a":
+//                         case "e":
+//                         case "i":
+//                         case "o":
+//                         case "u":
+//                             count++;
+//                     }
+//             }
+//         }
+        
+//     }
+//     return count;
+// }
+// var str = prompt("Enter any string");
+// var count = findOccurances(str);
+// document.write("The Entered String : '" + str + "'<br>")
+// document.write("The no. of occurences of any two vowels in succession : " + count);
+
+
+// Question 08
+// var dist = +prompt("Enter distance b/w two cities in km.")
+// function calcMeter() {
+//     var meter = dist * 1000
+//     document.write("KM to Meters : " + meter)
+// }
+// function calcFeet() {
+//     var feet = dist * 3280.84
+//     document.write("<br> KM to Feets : " + feet)
+// }
+// function calcInch() {
+//     var inch = dist * 39370.1
+//     document.write("<br> KM to Inch : " + inch)
+// }
+// function calcCenti() {
+//     var centimeter = dist * 100000
+//     document.write("<br> KM to Centimeters" + centimeter)
+// }
+// calcMeter()
+// calcFeet()
+// calcInch()
+// calcCenti()
+
+
 
 // Chapter 43 - 48
 
@@ -1628,3 +1798,5 @@
 //     var input = document.getElementById("input")
 //     input.value = eval(+input.value - 1)
 // }
+
+
